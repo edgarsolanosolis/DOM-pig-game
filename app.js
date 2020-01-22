d3dd32359dcf2ae6a428844e761ef04397654565
edgarsolanosolis <bolivarsolano@hotmail.com>
2020-01-22 16:32:38 -0600

    Video 48
    Avance del video numero 48 de la clase de JavaScript

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ GAME RULES:
 - El primer jugador en alcanzar 100 puntos en el puntaje GLOBAL gana el juego
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+//Vamos a crear los dados...
+dice = Math.floor(Math.random() * 7); //esto me dara un numero entre el 0 al 6
+//console.log(dice);
+
+//document.querySelector('#current-' + activePlayer).textContent = dice; (((Con est metodo se cargaria l numero normal)))
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; //con este metodo el numero lo cargamos en cursiva desde el javascript al HTML
+
+
+//Ahora creamos una variable y le almacenamos un digito del HTML hasta aqui.
+var x = document.querySelector('#score-0').textContent;
+
+document.querySelector('.dice').style.display = 'none'; //oculto la imagen del dado cuando cargo la imagen...
+
+
+
+
+
+
+
