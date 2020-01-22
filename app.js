e5331a3a170b4112faafa985227d2b37b1bde7ee
edgarsolanosolis <bolivarsolano@hotmail.com>
2020-01-21 21:08:42 -0600

    Inicio del curso
    Complementos iniciales para iniciar el curso.

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+GAME RULES:
+
+- El juego tiene 2 jugadores, jugando en rondas.
+- En cada turno, un jugador tira un dado tantas veces como lo desee. Cada resultado se agrega a su puntaje REDONDO
+- PERO, si el jugador saca un 1, toda su puntuación REDONDA se pierde. Después de eso, es el turno del siguiente jugador
+- El jugador puede elegir 'Hold', lo que significa que su puntaje REDONDO se agrega a su puntaje GLBAL. Después de eso, es el turno del siguiente jugador
+- El primer jugador en alcanzar 100 puntos en el puntaje GLOBAL gana el juego
+
+*/
